d6c42c839eb078355778443a466e8fd689ced05d
fflorgonzalo <82854512+fflorgonzalo@users.noreply.github.com>
2022-09-01 11:44:52 -0300

    Update Primer Entrega - Florencia Gonzalo.sql

--- Primer Entrega - Florencia Gonzalo.sql
+++ Primer Entrega - Florencia Gonzalo.sql
@@ -4,9 +4,9 @@ CREATE TABLE `centros` (
   `nombre` varchar(45) NOT NULL DEFAULT '30',
   `apellido` varchar(45) NOT NULL DEFAULT '30',
   `telefono` int NOT NULL DEFAULT '10',
-  `id_cuenta` int DEFAULT NULL,
   PRIMARY KEY (`id_centro`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 CREATE TABLE `contactos_de_confianza` (
   `id_contacto_confianza` int NOT NULL AUTO_INCREMENT,
@@ -19,17 +19,22 @@ CREATE TABLE `contactos_de_confianza` (
   PRIMARY KEY (`id_contacto_confianza`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+
 CREATE TABLE `cuentas_bancarias` (
   `id_cuenta` int NOT NULL AUTO_INCREMENT,
   `id_tipo_cuenta` varchar(45) NOT NULL DEFAULT '50',
+  `id_institucion` int NOT NULL,
+  `id_centro` int NOT NULL,
   PRIMARY KEY (`id_cuenta`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 CREATE TABLE `instituciones_bancarias` (
   `id_institucion` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL DEFAULT '60',
   PRIMARY KEY (`id_institucion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 CREATE TABLE `managers` (
   `id_manager` int NOT NULL AUTO_INCREMENT,
@@ -42,7 +47,7 @@ CREATE TABLE `managers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-CREATE TABLE `MIEMBROS` (
+CREATE TABLE `miembros` (
   `id_miembro` int NOT NULL AUTO_INCREMENT,
   `mail` varchar(45) NOT NULL DEFAULT '50',
   `nombre` varchar(45) NOT NULL DEFAULT '30',
@@ -62,7 +67,6 @@ CREATE TABLE `tipos_cuenta_bancaria` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-
 CREATE TABLE `transacciones` (
   `id_centro` int NOT NULL,
   `id_vendedor` int NOT NULL,
@@ -78,3 +82,5 @@ CREATE TABLE `vendedores` (
   `nombre` varchar(45) NOT NULL DEFAULT '50',
   PRIMARY KEY (`id_vendedor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
